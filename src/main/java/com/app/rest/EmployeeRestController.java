ceccb2d729e58809527b4aedfe44593c83393e30
Dev <Dev1@gmail.com>
2020-07-23 11:18:42 +0530

    WH-112 first project

---
+++ src/main/java/com/app/rest/EmployeeRestController.java
@@ -0,0 +1,18 @@
+package com.app.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeRestController {
+	@Value("${server.port}")
+	private int portNo;
+	
+	@GetMapping("/employee")
+	public String getEmp()
+	{
+		return "From Employee"+portNo;
+	}
+
+}
